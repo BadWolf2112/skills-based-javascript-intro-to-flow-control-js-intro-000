3b008c8a2e1b5ae1ebf7c098efa07d8edd6aa3ed
James <jamesalexandersmith2112@gmail.com>
2020-12-23 17:30:47 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,13 +1,15 @@
 function basicTeenager(age) {
-
+if (age = 13, 14, 15, 16, 17, 18, 19) 
+  return("You are a teenager!") 
 }
-
 function teenager(age) {
-
+if (age = 13, 14, 15, 16, 17, 18, 19) 
+  return("You are a teenager!") 
 }
 
 function ageChecker(age) {
-
+if (age = 13, 14, 15, 16, 17, 18, 19) 
+  return("You are a teenager!") 
 }
 
 function ternaryTeenager(age) {
